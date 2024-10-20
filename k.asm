64da32daf551c9910adc5226fab9ef9b60699be2
“Saymon <srrasador@ucs.br>
2024-10-20 20:08:27 -0300

    Modificação depois do professor mudar o jeito de desenhar as naves

--- k.asm
+++ k.asm
@@ -1,273 +1,238 @@
 .model small
-.stack 100H         ; define uma pilha de 256 bytes (100H)
+.stack 100H         ; Pilha de 256 bytes
+
 .data
-    VIDEO_SEGMENT dw 0A000h
+    VIDEO_SEGMENT dw 0A000h 
     
-    NAVE_X dw 2Fh          ;Posicao inicial X da nave principal
-    NAVE_Y dw 64h          ;Posicao inicial Y da nave principal
-    ALIEN_X dw 104h        ;Posicao inicial X da nave alienigena
-    ALIEN_Y dw 64h         ;Posicao inicial Y da nave alienigena
-    NAVE_HEIGHT dw 9       ;Altura da nave (9 pixels)
-    NAVE_WIDTH dw 15       ;Largura da nave (15 pixels)
-    SCREEN_HEIGHT dw 200   ;Altura da tela (320x200 modo gr?fico)
-    VELOCIDADE_NAVE dw 4   ; Velocidade da nave principal
+    NAVE_ALIADA db 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0h, 0h, 0h, 0h, 0h, 0h 
+              db 0h,   0H, 0Fh, 0Fh, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h  
+              db 0h,   0h, 0Fh, 0Fh, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h  
+              db 0h,   0h, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h 
+              db 0h,   0h, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh
+              db 0h,   0h, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h 
+              db 0h,   0h, 0Fh, 0Fh, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h  
+              db 0h,   0h, 0Fh, 0Fh, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h 
+              db 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0Fh, 0h, 0h, 0h, 0h, 0h, 0h
+              
+    NAVE_ALIENIGENA db 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 09h, 09h, 09h, 09h, 09h, 09h 
+              db 0h, 0H, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 09h, 09h, 0h, 0h, 0h, 0h  
+              db 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h  
+              db 0h, 0h, 0h, 0h, 09h, 09h, 09h, 09h, 09h, 0h, 0h, 0h, 0h, 0h, 0h 
+              db 09h, 09h, 09h, 09h, 09h, 09h, 09h, 09h, 09h, 09h, 09h, 09h, 0h, 0h, 0h
+              db 0h, 0h, 0h, 0h, 09h, 09h, 09h, 09h, 09h, 0h, 0h, 0h, 0h, 0h, 0h 
+              db 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h  
+              db 0h, 0H, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 09h, 09h, 0h, 0h, 0h, 0h  
+              db 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 0h, 09h, 09h, 09h, 09h, 09h, 09h 
+                    
+    NAVE_PRINCIPAL_X dw 2Fh            ; Posicao X da nave principal
+    NAVE_PRINCIPAL_Y dw 50h           ; Posicao Y da nave principal
+    NAVE_Y_START dw 20               ; Posicao inicial Y da primeira nave fixa
+    SPACING     dw 20               ; Espacamento entre as naves
+    COR_NAVES db 0Eh, 05h, 09h, 0Ah, 0Bh, 0Ch, 0Dh, 04h
+    VELOCIDADE dw 2   
     
-    NAVE_FIXA_Y dw 0, 19, 38, 57, 76, 95, 114, 133 ;coordenadas das 8 naves fixas
-    NAVE_CORES db 9h, 0Ah, 6h, 5h, 0Eh, 7h, 0Dh, 0Ch ; Cores das naves
 .code
 
-
-
-DRAW_ALL_NAVES proc
-    ; Desenhar as 8 naves
-    mov CX, 8          
-    xor BX, BX 
-    
-    DRAW_LOOP:
-    mov AX, BX           
-    shl AX, 1            ; Cada entrada em NAVE_FIXA_Y tem 2 bytes, por isso desloca
-    mov SI, offset NAVE_FIXA_Y  
-    add SI, AX           ; Calcula o endereco Y da nave e passa para DX
-    mov DX, [SI]         ; DX e a posicao Y das naves fixas 
-    
-    mov AL, [NAVE_CORES + BX]   ; Cor da nave fixa
-    
-    push CX              ; Salvar contador do loop (CX)
-    call DRAW_NAVE       ; Desenhar a nave fixa
-    pop CX
-    inc BX               ; Proxima nave fixa
-    loop DRAW_LOOP       ; Decrementa CX=8 ate CX=0
-    
-    
-    ; Desenhar a nave principal
-    mov CX, NAVE_X       
-    mov DX, NAVE_Y       
-    mov AL, 0Fh          ; Cor da nave principal
-    call DRAW_NAVE      
-
+SET_VIDEO_MODE proc
+    ; Configurar modo de video 13h (320x200, 256 cores)
+    mov AX, 0013h    
+    int 10h         
     ret
 endp
 
 
 
 DRAW_NAVE proc
-    mov AX, VIDEO_SEGMENT
-    mov ES, AX    ; Configura ES para apontar para o segmento de video
+    push BX
+    push CX
+    push DX
+    push SI
+    push AX
+      
+    ; ES aponta para o segmento de video
+    mov AX, VIDEO_SEGMENT  
+    mov ES, AX
+    
+    ; Calcular posicao da nave --> CX=X e DX=Y
+    mov AX, 320  
+    mul DX            ; Multiplica Y por 320 para achar a linha(Y)
+    add AX, CX        ; Soma o deslocamento para achar a coluna(X)
+    mov DI, AX        ; DI aponta para o pixel da nave
 
-    ; Calcular o endereco de video baseado em X e Y (posicao da nave)
+    ; DS aponta para o segmento de dados
+    mov AX, @DATA
+    mov DS, AX        
+    
+ 
+    
+    ; Recupera tipo de nave (AH) e a cor da nave (AL)
+    pop AX            
+    
+    ; AH e o tipo da nave
+    cmp AH, 1           
+    je DRAW_ALIADA
+    cmp AH, 2           
+    je DRAW_ALIENIGENA
+    
+    DRAW_ALIADA:
+    mov SI, OFFSET NAVE_ALIADA  ; Aponta para o desenho da nave aliada
+    jmp CONTINUEE
+
+    DRAW_ALIENIGENA:
+    mov SI, OFFSET NAVE_ALIENIGENA ; Aponta para o desenho da nave alien?gena
+    
+   
+    ; AL e a cor da nave
+    CONTINUEE:
+    mov DL, AL          ; Faz uma c?pia da cor no registrador DL
+    push DX             ; Salva a posi??o Y da nave
+       
+  
+    
+    ; Definir limites do desenho da nave
+    mov CX, 9           
+    mov BX, 15          
+    
+    CLD                 ; Limpa o flag de direcao (incrementa SI/DI)
+    DRAW_LINE:
+    push CX             ; Salvar contador de linhas
+    mov CX, BX          ; Contador CX recebe o valor 15 (numero de pixels)
+        
+    DRAW_PIXEL:
+    lodsb               ; Carrega o pixel da nave (apontado por SI) em AL
+    cmp AL, 0Fh         ; Verifica se o pixel e 0FH
+    jne NOT_0Fh        
+    mov AL, DL          ; DL possui o valor original de AL (cor)
+    
+    NOT_0Fh:
+    stosb               ; Aplica a cor de AL no pixel da nave (ES:DI)
+    loop DRAW_PIXEL     ; loop ate acabar a linha 
+
+    pop CX              ; Contador CX passa a valer 9 (altura da nave)
+    ; Ajustar DI para pular para proxima linha
     mov AX, 320
-    mul DX  
-    add AX, CX
-    mov DI, AX     ; DI recebe a posicao de memoria de video
+    sub AX, BX
+    add DI, AX          ; Mover DI para a proxima linha
+    loop DRAW_LINE      ; loop ate acabar a coluna
     
-    
-    ; Desenhar a nave usando a cor passada em AL
-    mov CX, 9        ; Numero de pixels a desenhar na primeira linha
-    rep stosb        ; Desenha a primeira linha da nave
-    add DI, 320 - 7  ; Avanca 1 linha e ajusta a posicao
-
-    mov CX, 2        ; Segunda linha
-    rep stosb        
-    add DI, 320 - 2
-    
-    mov CX, 2        ; Terceira linha
-    rep stosb        
-    add DI, 320 - 2
-
-    mov CX, 6        ; Quarta linha
-    rep stosb        
-    add DI, 320 - 6
-
-    mov CX, 13       ; Quinta linha
-    rep stosb        
-    add DI, 320 - 13
-
-    mov CX, 6        ; Sexta linha
-    rep stosb        
-    add DI, 320 - 6
-
-    mov CX, 2        ; Setima linha
-    rep stosb        
-    add DI, 320 - 2
-
-    mov CX, 2        ; Oitava linha
-    rep stosb        
-    add DI, 320 - 4
-
-    mov CX, 9        ; Nona linha
-    rep stosb
-
+    pop DX
+    pop SI
+    pop DX
+    pop CX
+    pop BX
     ret
 endp
 
 
 
-DRAW_ALIEN proc
-    mov AX,VIDEO_SEGMENT
-    mov ES,AX   ; Configura ES para apontar para o segmento de video
+DRAW_8_NAVES proc
+    mov CX, 8                ; Contador CX do numero de naves fixas
+    mov DX, NAVE_Y_START     ; Posicao Y inicial das naves fixas
 
-    ; Calcular o endereco de video baseado em X e Y (posicao da nave)
-    mov BX, ALIEN_Y 
-    mov AX, 320
-    mul BX           ; AX = NAVE_Y * 320 (cada linha tem 320 pixels)
-    add AX, ALIEN_X 
-    mov DI, AX       ; DI recebe a posicao de memoria de video
+    DRAW_LOOP:
+    push DX          
+    push CX             
+
+    ; Definir a cor
+    mov SI, CX                 ; Valor de CX determina a posicao de COR_NAVES
+    mov BX, OFFSET COR_NAVES
+    mov AL, [BX + SI]          ; Acessa a cor correspondente e guarda em AL
     
+    mov CX, 0           ; Posicao X=0
+    call DRAW_NAVE  
 
-    mov AL, 9h      ; Cor do pixel (branca)
+    pop CX              
+    pop DX              
+    add DX, SPACING     ; Quantidade de espacamento em DX (Y)
+    loop DRAW_LOOP
     
-    
-    ; Desenhar a nave usando a cor passada em AL
-    add DI, 9
-    mov CX, 6        ; Numero de pixels a desenhar na primeira linha
-    rep stosb        ; Desenha a primeira linha da nave
-    add DI, 320 - 6  ; Avanca 1 linha e ajusta a posicao
-
-    mov CX, 1        ; Segunda linha
-    rep stosb        
-    add DI, 320 - 7
-    
-    mov CX, 0        ; Terceira linha
-    rep stosb        
-    add DI, 320 - 0
-
-    mov CX, 5        ; Quarta linha
-    rep stosb        
-    add DI, 320 - 9
-
-    mov CX, 13       ; Quinta linha
-    rep stosb        
-    add DI, 320 - 8
-
-    mov CX, 5        ; Sexta linha
-    rep stosb        
-    add DI, 320 + 1
-
-    mov CX, 0        ; Setima linha
-    rep stosb        
-    add DI, 320 - 0
-
-    mov CX, 2        ; Oitava linha
-    rep stosb        
-    add DI, 320 - 2
-
-    mov CX, 6        ; Nona linha
-    rep stosb
-
     ret
 endp
 
 
 
+DRAW_NAVE_PRINCIPAL proc
+    mov DX, NAVE_PRINCIPAL_Y 
+    mov CX, NAVE_PRINCIPAL_X 
+    mov AL, 0FH
+    mov AH, 1
+    call DRAW_NAVE 
+
+    ret
+endp
 
 
+DRAW_NAVE_ALIENIGENA proc
+    mov DX, 80
+    mov CX, 160
+    mov AL, 09H
+    mov AH, 2
+    call DRAW_NAVE 
 
-MOVE_NAVE proc
-    ; Esperar por uma tecla pressionada
-    mov AH, 01h          ; Checar se uma tecla foi pressionada
-    int 16h              ; int do teclado
-    jz END_MOVE_NAVE     ; Se nenhuma tecla foi pressionada, sai do procedimento
-    mov AH, 00h          ; L? a tecla pressionada
-    int 16h              ; int do teclado
+    ret
+endp
 
-    cmp AH, 48h          ; C?digo da seta para cima (?)
+
+MOVE_NAVE_PRINCIPAL proc
+    ; Verifica se ha uma tecla pressionada
+    mov AH, 1          
+    int 16h             
+    jz NO_UPDATE       
+
+    ; Le codigo das teclas
+    mov AH, 0          
+    int 16h             
+    cmp AL, 'o'
     je MOVE_UP
-    cmp AH, 50h          ; C?digo da seta para baixo (?)
+    cmp AL, 'O'
+    je MOVE_UP
+    cmp AL, 'l'
     je MOVE_DOWN
-    jmp END_MOVE_NAVE    ; Se n?o for uma das teclas esperadas, sai do procedimento
-    
+    cmp AL, 'L'
+    je MOVE_DOWN
+
+    NO_UPDATE:
+    ret                 ; Nao houve atualizacao, retorna
+
     MOVE_UP:
-    call CLEAR_NAVE      ; Apaga a nave na posi??o atual
-    cmp NAVE_Y, 0        ; Verifica se a nave est? no limite superior
-    jle END_MOVE_NAVE    ; Se j? est? no topo, n?o faz nada
-    mov AX, VELOCIDADE_NAVE
-    sub NAVE_Y, AX       ; Move a nave para cima diminuindo Y
-    call DRAW_NAVE       ; Desenha a nave na nova posi??o
-    jmp END_MOVE_NAVE
+    mov AX, NAVE_PRINCIPAL_Y
+    cmp AX,20           ; LIMITE SUPERIOR
+    jle NO_UPDATE    
+    sub AX, VELOCIDADE                   ; Move a nave para cima
+    mov NAVE_PRINCIPAL_Y, AX    ; Atualiza posicao X
+    jmp DRAW            ; Apos mover, desenha a nave na nova posicao
 
     MOVE_DOWN:
-    call CLEAR_NAVE      ; Apaga a nave na posi??o atual
-    mov AX, NAVE_Y       ; Verificar se a nave est? no limite inferior da tela
-    add AX, NAVE_HEIGHT
-    cmp AX, SCREEN_HEIGHT; Verifica se a nave vai ultrapassar a tela
-    jge END_MOVE_NAVE    ; Se j? est? no limite inferior, n?o faz nada
-    mov AX, VELOCIDADE_NAVE
-    add NAVE_Y, AX       ; Move a nave para baixo aumentando Y
-    call DRAW_NAVE       ; Desenha a nave na nova posi??o
-    
-    END_MOVE_NAVE:
+    mov AX, NAVE_PRINCIPAL_Y
+    cmp AX, 160         ; LIMITE INFERIOR
+    jge NO_UPDATE      
+    add AX, VELOCIDADE          ; Move a nave para baixo
+    mov NAVE_PRINCIPAL_Y, AX    ; Atualiza posicao X
+
+    DRAW:
+    call DRAW_NAVE_PRINCIPAL  ; Redesenha a principal na nova posicao
     ret
 endp
 
 
-
-CLEAR_NAVE proc
-    ; Recebe a posi??o X e Y em NAVE_X, NAVE_Y
-    mov AX, VIDEO_SEGMENT
-    mov ES, AX   ; Configura ES para apontar para o segmento de v?deo
-
-    ; Calcular o endere?o de v?deo baseado em X e Y (posi??o da nave)
-    mov BX, NAVE_Y 
-    mov AX, 320
-    mul BX           ; AX = NAVE_Y * 320 (cada linha tem 320 pixels)
-    add AX, NAVE_X
-    mov DI, AX       ; DI recebe a posi??o de mem?ria de v?deo
-
-    ; Apagar a nave usando a cor preta (0)
-    mov AL, 0        ; Cor do fundo (preto)
-    mov CX, 9        ; N?mero de pixels a apagar na primeira linha
-    rep stosb        ; Apaga a primeira linha da nave
-    add DI, 320 - 7  ; Avan?a 1 linha e ajusta a posi??o
-
-    ; Apagar as linhas restantes da nave
-    mov CX, 2        
-    rep stosb        
-    add DI, 320 - 2
-
-    mov CX, 2        
-    rep stosb        
-    add DI, 320 - 2
-
-    mov CX, 6        
-    rep stosb        
-    add DI, 320 - 6
-
-    mov CX, 13       
-    rep stosb        
-    add DI, 320 - 13
-
-    mov CX, 6        
-    rep stosb        
-    add DI, 320 - 6
-
-    mov CX, 2        
-    rep stosb        
-    add DI, 320 - 2
-
-    mov CX, 2        
-    rep stosb        
-    add DI, 320 - 4
-
-    mov CX, 9        
-    rep stosb
-
-    ret
-endp
 
 
 
 INICIO:   
-    mov AX,@DATA
-    mov DS,AX 
-            
-    mov AX, 13h     ;Configurar modo gr?fico 320x200 com 256 cores
-    int 10h
+    mov AX,@DATA 
+    mov DS,AX
 
-   
-    WAIT_LOOP:      ; Loop infinito para manter o gr?fico na tela
-    call MOVE_NAVE  ; Mover a nave principal
-    call DRAW_ALL_NAVES
-    call DRAW_ALIEN
-    jmp WAIT_LOOP
-        
+    call SET_VIDEO_MODE
+    call DRAW_8_NAVES
+    call DRAW_NAVE_PRINCIPAL
+    call DRAW_NAVE_ALIENIGENA
+    
+    
+    MAIN_LOOP:
+    call MOVE_NAVE_PRINCIPAL
+
+    ; Aqui adicionamos o resto da logica do jogo (deteccao de colisao, etc.)
+
+    jmp MAIN_LOOP        
+
 end INICIO
